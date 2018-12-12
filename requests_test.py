9cbf2c449ffcadee18381285540cf8316a9eff94
Michelin <michael.kazitsin@yandex.ru>
2018-12-12 14:51:05 +0300

    web part

--- requests_test.py
+++ requests_test.py
@@ -1,11 +1,15 @@
 import requests
 from pprint import pprint
+
 app_id = '07fd9e87806a3b778c76e0a21639f307'
+
 city = input()
+
 url = 'https://api.openweathermap.org/data/2.5/weather?q={}&APPID={}&units=metric'.format(
     city, app_id)
+
 data = requests.get(url).json()
-pprint(data)
+
 temp = str(data['main']['temp']) + ' °С'
 vlazhnost = str(data['main']['humidity']) + ' %'
 davlenie = str(data['main']['pressure'] / (4/3)) + ' мм.рт.ст.'
